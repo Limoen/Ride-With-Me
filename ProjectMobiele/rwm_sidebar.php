483b0176b7e291b295b4b980e0dd91a5d9db1704
Sebastiaan Sebille <sebastiaan.sebille@gmail.com>
2013-05-19 13:22:18 +0200

    Sidebar menu added

---
+++ ProjectMobiele/rwm_sidebar.php
@@ -0,0 +1,18 @@
+<div id="sidebar">
+	<div data-theme="a" data-role="header">    
+        <h3>
+            Ride with.me
+        </h3>
+        <header>
+			<nav id="mobile-bar"></nav>
+				
+			<nav id="main-nav">
+				<ul>
+					<li><a href="rwm_loggedin.php" >Search ride</a></li>
+					<li><a href="rwm_loggedin.php">Create ride</a></li>
+					<li><a href="rwm_loggedin.php">Your rides</a></li>
+				</ul>
+			</nav>
+		</header>
+	</div>
+</div>
